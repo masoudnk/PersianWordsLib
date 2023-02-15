650d9f121a708d3024f263ce1bc1f23c1b42486a
masoudnk <masoudnk2@gmail.com>
2023-02-15 16:21:03 +0330

    1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='mnk_persian_words',
     packages=find_packages(include=['mnk_persian_words']),
-    version='0.1.6',
+    version='0.1.7',
     description='Creates REALLY random Persian words',
     author='Masoud Najafzadeh Kalat',
     license='MIT',
